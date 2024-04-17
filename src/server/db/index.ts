137022926b6e7cd12789e932f52207f38a5a1a73
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-04-17 22:05:23 +0530

    pls fix pls pls

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import { env } from "~/env";
 import * as schema from "./schema";
 
 /**
@@ -12,7 +11,7 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!);
+if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
